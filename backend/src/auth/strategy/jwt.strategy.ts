3657d48c125edce418eee3d7181d38e55bdaadc3
Hector Barazorda <ph.barazorda@gmail.com>
2026-06-17 22:12:28 -0500

    fix(backend): add non-null assertion to jwtSecret in JwtStrategy

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtSecret,
+      secretOrKey: jwtSecret!,
     });
   }
 
