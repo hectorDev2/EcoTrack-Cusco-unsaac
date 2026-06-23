3f72ade45f73aa0bc2bcfa8dbf12cb6465649648
Hector Barazorda <ph.barazorda@gmail.com>
2026-06-23 06:07:58 -0500

    fix(api): normalize API URL by removing trailing slashes

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 if (!apiUrl) {
   throw new Error('NEXT_PUBLIC_API_URL environment variable is required');
 }
-const API_URL = apiUrl;
+const API_URL = apiUrl.replace(/\/+$/, '');
 
 export class ApiClientError extends Error {
   constructor(
